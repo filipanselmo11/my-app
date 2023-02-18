83c2f1fba85b2097e520c411edc1b76d09e4604f
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2023-02-18 09:43:02 -0300

    Todo app com react ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TodoTask from "./components/TodoTask/TodoTask";
+import { ITask } from "./interfaces";
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import './styles/styles.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [task, setTask] = useState("")
+	const [todoList, setTodoList] = useState<ITask[]>([])
+
+	function addTask(): void {
+		if (task === "") {
+			toast.error("Digite alguma task")
+		} else {
+			const idRandom = (num: number) => Math.floor(Math.random() * num)
+			console.log(idRandom(10))
+
+			const newTask = { id: idRandom(999999999999), nameTask: task }
+
+			setTodoList([...todoList, newTask])
+
+			toast.success("Task cadastrada com sucesso");
+		}
+	}
+
+	function deleteTask(deleteTaskById: number): void {
+		setTodoList(todoList.filter((taskName) => taskName.id !== deleteTaskById))
+	}
+
+	return (
+		<div className="App">
+
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover={false} />
+
+			<header>
+
+				<h2>Lists</h2>
+
+				<input
+					type="text" autoComplete="off"
+					placeholder="Escrever task..."
+					name="task"
+					className="input"
+					value={task}
+					onChange={(e) => setTask(e.target.value)}
+				/>
+
+				<button onClick={addTask} type="submit" className="btn-header">Adicionar Task</button>
+			</header>
+			<div className="line"></div>
+			{todoList.map((task, key) => (
+				<TodoTask key={key} task={task} deleteTask={deleteTask} />
+			))}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TodoTask/TodoTask.tsx
@@ -0,0 +1,24 @@
+
+import { ITask } from '../../interfaces';
+import './styles.css'
+
+interface TaskProps {
+    task: ITask,
+    deleteTask(deleteTaskById: number): void
+}
+
+function TodoTask({ task, deleteTask }: TaskProps) {
+	return (
+		<div className="card">
+			<div>
+                <p>{task.nameTask}</p>
+            </div>
+
+            <div className="line2" >
+            <span onClick={() => deleteTask(task.id)} className="btn-card">X</span>
+            </div>
+		</div>
+	);
+}
+
+export default TodoTask;
